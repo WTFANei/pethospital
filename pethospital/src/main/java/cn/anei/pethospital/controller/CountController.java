efc2861f078e821bf65eed95575dc4d5ed255b49
WTFANei <317249580@qq.com>
2021-04-04 22:12:56 +0800

    1

--- pethospital/src/main/java/cn/anei/pethospital/controller/CountController.java
+++ pethospital/src/main/java/cn/anei/pethospital/controller/CountController.java
@@ -21,40 +21,40 @@ public class CountController {
 
     @PostMapping("/getCountItemForUser")
     public ResultVO getCountItemForUser(@RequestParam("userId") String userId, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, HttpSession session) {
-//        Object sess = session.getAttribute("user");
-//        if(null == sess){
-//            return ResultVOUtil.error(1,"用户请先登录！"); //越权操作，跳转到用户登录界面
-//        }
+        Object sess = session.getAttribute("user");
+        if(null == sess){
+            return ResultVOUtil.error(1,"用户请先登录！"); //越权操作，跳转到用户登录界面
+        }
         List<CountData> countData = countService.getCountItemForUser(userId, startDate, endDate);
         return countData != null ? ResultVOUtil.success(countData) : ResultVOUtil.error(1, "统计信息获取失败！");
     }
 
     @PostMapping("/getCountMedicineForUser")
     public ResultVO getCountMedicineForUser(@RequestParam("userId") String userId, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, HttpSession session) {
-//        Object sess = session.getAttribute("user");
-//        if(null == sess){
-//            return ResultVOUtil.error(1,"用户请先登录！"); //越权操作，跳转到用户登录界面
-//        }
+        Object sess = session.getAttribute("user");
+        if(null == sess){
+            return ResultVOUtil.error(1,"用户请先登录！"); //越权操作，跳转到用户登录界面
+        }
         List<CountData> countData = countService.getCountMedicineForUser(userId, startDate, endDate);
         return countData != null ? ResultVOUtil.success(countData) : ResultVOUtil.error(1, "统计信息获取失败！");
     }
 
     @PostMapping("/getCountItemForAdmin")
     public ResultVO getCountItemForAdmin(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, HttpSession session) {
-//        Object sess = session.getAttribute("admin");
-//        if(null == sess){
-//            return ResultVOUtil.error(1,"管理员请先登录！"); //越权操作，跳转到管理员登录界面
-//        }
+        Object sess = session.getAttribute("admin");
+        if(null == sess){
+            return ResultVOUtil.error(1,"管理员请先登录！"); //越权操作，跳转到管理员登录界面
+        }
         List<CountData> countData = countService.getCountItemForAdmin(startDate, endDate);
         return countData != null ? ResultVOUtil.success(countData) : ResultVOUtil.error(1, "统计信息获取失败！");
     }
 
     @PostMapping("/getCountMedicineForAdmin")
     public ResultVO getCountMedicineForAdmin(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate, HttpSession session) {
-//        Object sess = session.getAttribute("admin");
-//        if(null == sess){
-//            return ResultVOUtil.error(1,"管理员请先登录！"); //越权操作，跳转到管理员登录界面
-//        }
+        Object sess = session.getAttribute("admin");
+        if(null == sess){
+            return ResultVOUtil.error(1,"管理员请先登录！"); //越权操作，跳转到管理员登录界面
+        }
         List<CountData> countData = countService.getCountMedicineForAdmin(startDate, endDate);
         return countData != null ? ResultVOUtil.success(countData) : ResultVOUtil.error(1, "统计信息获取失败！");
     }
