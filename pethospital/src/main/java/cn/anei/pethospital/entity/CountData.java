7bfa0d6867bff97b0b82d58fa9b3753d00f045f0
WTFANei <317249580@qq.com>
2021-04-04 20:55:18 +0800

    1

---
+++ pethospital/src/main/java/cn/anei/pethospital/entity/CountData.java
@@ -0,0 +1,20 @@
+package cn.anei.pethospital.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+
+@Data
+@Entity
+public class CountData {
+
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "count")
+    private Long count;
+    @Column(name = "name")
+    private String name;
+}
