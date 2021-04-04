bc9fac11fd5844cb53569d6142f5b78973e23080
WTFANei <317249580@qq.com>
2021-04-04 20:27:31 +0800

    1

--- pethospital/src/文档/pethospital.sql
+++ pethospital/src/文档/pethospital.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2021-04-04 16:31:16
+Date: 2021-04-04 20:27:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,6 +30,21 @@ CREATE TABLE `admin` (
 
 -- ----------------------------
 -- Records of admin
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for count_data
+-- ----------------------------
+DROP TABLE IF EXISTS `count_data`;
+CREATE TABLE `count_data` (
+  `id` varchar(255) NOT NULL,
+  `count` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of count_data
 -- ----------------------------
 
 -- ----------------------------
@@ -66,7 +81,11 @@ CREATE TABLE `item` (
 -- ----------------------------
 -- Records of item
 -- ----------------------------
-INSERT INTO `item` VALUES ('1', '1', '1', '0');
+INSERT INTO `item` VALUES ('1', '毛皮护理', '毛皮护理', '0');
+INSERT INTO `item` VALUES ('2', '口腔护理', '口腔护理', '0');
+INSERT INTO `item` VALUES ('3', '头皮护理', '头皮护理', '0');
+INSERT INTO `item` VALUES ('4', '修剪护理', '修剪护理', '0');
+INSERT INTO `item` VALUES ('5', '健康护理', '健康护理', '0');
 
 -- ----------------------------
 -- Table structure for medicine
@@ -86,6 +105,10 @@ CREATE TABLE `medicine` (
 -- ----------------------------
 -- Records of medicine
 -- ----------------------------
+INSERT INTO `medicine` VALUES ('1', 'A药', null, '99', '10.00', '0');
+INSERT INTO `medicine` VALUES ('2', 'B药', null, '99', '5.00', '0');
+INSERT INTO `medicine` VALUES ('3', 'C药', null, '99', '3.00', '0');
+INSERT INTO `medicine` VALUES ('4', 'D药', null, '99', '100.00', '0');
 
 -- ----------------------------
 -- Table structure for orderinfo
@@ -108,12 +131,14 @@ CREATE TABLE `orderinfo` (
 -- ----------------------------
 -- Records of orderinfo
 -- ----------------------------
-INSERT INTO `orderinfo` VALUES ('1', '1', '1', '1', '1', '123123', '2021-04-02 23:32:39', null, '0', '0');
-INSERT INTO `orderinfo` VALUES ('2', '2', '3', '4', '5', '123123', '2021-04-03 00:22:51', null, '0', '0');
-INSERT INTO `orderinfo` VALUES ('3', '1', '2', '3', '4', '123123', '2021-04-03 00:23:08', null, '0', '0');
-INSERT INTO `orderinfo` VALUES ('4', '1', '1', '5', '7', '123123', '2021-04-03 00:25:31', null, '0', '0');
-INSERT INTO `orderinfo` VALUES ('402880e6789baa9801789baabfd50000', '1', '1', '1', '1', null, '2021-04-04 14:56:02', '2021-04-04 14:56:02', '0', '0');
-INSERT INTO `orderinfo` VALUES ('402880e6789baa9801789bac0b200001', '1', '1', '1', '1', null, '2021-04-04 14:57:27', '2021-04-04 14:57:27', '0', '1');
+INSERT INTO `orderinfo` VALUES ('1', '1', '1', '1', '1', '123123', '2021-04-02 23:32:39', '2021-04-04 18:57:21', '1', '0');
+INSERT INTO `orderinfo` VALUES ('2', '2', '3', '4', '5', '123123', '2021-04-03 00:22:51', '2021-04-04 18:57:19', '1', '0');
+INSERT INTO `orderinfo` VALUES ('3', '1', '2', '3', '4', '123123', '2021-04-03 00:23:08', '2021-04-03 18:57:16', '1', '0');
+INSERT INTO `orderinfo` VALUES ('4', '1', '1', '5', '7', '123123', '2021-04-03 00:25:31', '2021-04-03 18:57:14', '1', '0');
+INSERT INTO `orderinfo` VALUES ('402880e6789baa9801789baabfd50000', '2', '1', '1', '1', null, '2021-04-04 14:56:02', '2021-04-04 14:56:02', '1', '0');
+INSERT INTO `orderinfo` VALUES ('402880e6789baa9801789bac0b200001', '1', '1', '1', '1', null, '2021-04-04 14:57:27', '2021-04-04 14:57:27', '1', '1');
+INSERT INTO `orderinfo` VALUES ('5', '3', '1', '1', '1', '12312344', '2021-04-04 18:38:58', '2021-04-04 18:57:23', '1', '0');
+INSERT INTO `orderinfo` VALUES ('6', '4', '1', '6', '2', '123123', '2021-04-04 18:39:29', '2021-04-04 18:57:26', '1', '0');
 
 -- ----------------------------
 -- Table structure for pet
@@ -141,9 +166,10 @@ DROP TABLE IF EXISTS `prescription`;
 CREATE TABLE `prescription` (
   `prescription_id` varchar(32) NOT NULL,
   `order_id` varchar(32) NOT NULL,
-  `medicine_id` varchar(32) DEFAULT NULL,
-  `medicine_num` int(4) DEFAULT NULL,
+  `medicine_id` varchar(32) NOT NULL,
+  `medicine_num` int(4) NOT NULL,
   `count_price` float(18,2) NOT NULL,
+  `prescription_time` datetime NOT NULL,
   `status` int(1) NOT NULL DEFAULT '0' COMMENT '0-生效 1-失效',
   PRIMARY KEY (`prescription_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -151,6 +177,11 @@ CREATE TABLE `prescription` (
 -- ----------------------------
 -- Records of prescription
 -- ----------------------------
+INSERT INTO `prescription` VALUES ('1', '1', '1', '1', '10.00', '2021-04-04 20:08:21', '0');
+INSERT INTO `prescription` VALUES ('2', '1', '2', '2', '10.00', '2021-04-04 20:09:34', '0');
+INSERT INTO `prescription` VALUES ('3', '2', '1', '2', '20.00', '2021-04-04 20:09:49', '0');
+INSERT INTO `prescription` VALUES ('4', '3', '3', '3', '9.00', '2021-04-04 20:13:20', '0');
+INSERT INTO `prescription` VALUES ('5', '4', '4', '4', '400.00', '2021-04-04 20:14:53', '0');
 
 -- ----------------------------
 -- Table structure for user
